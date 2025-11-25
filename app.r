788afea900257628dc680ab26418ea50d6050ac9
Fox Walczak <fox.walczak2023@my.ntu.ac.uk>
2025-11-25 12:49:14 +0000

    refactor,feat: create functions; make reactive filtered data set; use aggregate function
    refactor: create min/max date funcs, load_data, costs, and revenue funcs
    
    refactor: use aggregate instead of manually aggregating sum by country
    
    feat: introduce reactive data frame

--- app.r
+++ app.r
@@ -7,31 +7,43 @@ library("DataExplorer") # ""
 library("plotly")       # Widgets
 library("tidyverse")    # Core
 
-# Load Data Set
-online_retail_file_path <- "OnlineRetail.csv"
-online_retail <- read_csv(online_retail_file_path)
-#Fix Date Format
-online_retail$date <- as.Date(online_retail$InvoiceDate, format="%m/%d/%Y %H:%M")
-online_retail$date_time <- as_datetime(online_retail$InvoiceDate, format="%m/%d/%Y %H:%M")
-# Calculate Invoice Amount
-online_retail$InvoiceAmount <- online_retail$Quantity * online_retail$UnitPrice
-# Split Costs and Revenue
-costs <- online_retail[online_retail$InvoiceAmount < 0,]
-revenue <- online_retail[online_retail$InvoiceAmount >= 0,]
+load_data <- function() {
+  online_retail <- readr::read_csv("OnlineRetail.csv")
+ 
+  dplyr::rename(online_retail, raw_date = InvoiceDate)
+  online_retail$date <- as.Date(online_retail$raw_date, format="%m/%d/%Y %H:%M")
+  online_retail$date_time <- lubridate::as_datetime(online_retail$raw_date, format="%m/%d/%Y %H:%M")
+ 
+  online_retail$InvoiceAmount <- online_retail$Quantity * online_retail$UnitPrice
 
-# Prepare Pie Chart
-countries <- unique(online_retail$Country)
-sum_by_country <- c()
-for(i in 1:length(countries)){
-  sum_by_country[i] <- sum(online_retail[online_retail$Country==countries[i],]$InvoiceAmount)
+  return(online_retail)
 }
-piec <- data.frame(countries, sum_by_country)
 
-min_date <- min(online_retail$date_time)
-max_date <- max(online_retail$date_time)
+# Split Costs and Revenue
+get_costs <- function(df){
+  return(df %>%
+           dplyr::pull(InvoiceAmount) %>%
+           dplyr::filter(InvoiceAmount < 0)
+         )}
+get_revenue <- function(df){
+  return(df %>%
+           dplyr::pull(InvoiceAmount) %>%
+           dplyr::filter(InvoiceAmount > 0)
+         )}
+min_date <- function(df) {
+  return(df %>%
+           dplyr::pull(date) %>%
+           min()
+  )}
+max_date <- function(df) {
+  return(df %>%
+           dplyr::pull(date) %>%
+           max()
+  )}
+
 # UI
 
-ui <- navbarPage(
+ui <- shiny::navbarPage(
   title = "Bundle and Promote",
   theme = bslib::bs_theme(version=4,bootswatch="minty"),
   tabPanel(
@@ -48,9 +60,9 @@ ui <- navbarPage(
         shiny::sliderInput(
           inputId="date_range",
           label="Date Range",
-          min=min_date,
-          max=max_date,
-          value=c(min_date, max_date),
+          min=min_date(online_retail),
+          max=max_date(online_retail),
+          value=c(min_date(online_retail), max_date(online_retail)),
           step=1,
           ticks=TRUE,
           dragRange=TRUE
@@ -83,21 +95,31 @@ ui <- navbarPage(
 # SERVER
 
 server <- function(input, output) {
-  #rv <- reactiveValues()
-  #observe({
-  #  rv$data_set <- data_list %>% pluck(input$dataset_choice)
-  #})
+  reactive({
+    load_data()
+  })
+  rv <- reactiveValues()
+  observe({
+    rv$min_date <- dplyr::pull(input, date_range[1])
+    rv$max_date <- dplyr::pull(input, date_range[2])
+  })
+  observe({
+    # orf = online retail filtered
+      rv$orf <- online_retail %>% dplyr::filter(date >= rv$min_date &
+                                                  date <= rv$max_date)
+  })
   output$revenue_over_time <- renderPlot({
-    ggplot(revenue, mapping=aes(x=date_time,y=InvoiceAmount)) + geom_line()
+    ggplot(get_revenue(rv$orf), mapping=aes(x=date_time,y=InvoiceAmount)) + geom_line()
   })
   output$costs_over_time <- renderPlot({
-    ggplot(costs, mapping=aes(x=date_time,y=InvoiceAmount)) + geom_line()
+    ggplot(get_costs(rv$orf), mapping=aes(x=date_time,y=InvoiceAmount)) + geom_line()
   })
   output$money_by_location <- renderPlot({
-    ggplot(online_retail, mapping=aes(x=date_time,y=InvoiceAmount,colour=Country)) + geom_line()
+    ggplot(rv$orf, mapping=aes(x=date_time,y=InvoiceAmount,colour=Country)) + geom_line()
   })
   output$revenue_by_country <- renderPlot({
-    ggplot(piec, mapping=aes(x=countries, y=sum_by_country, fill=countries)) +
+    country_data_frame <- stats::aggregate(InvoiceAmount~Country, get_revenue(rv$orf), sum)
+    ggplot(country_data_frame, mapping=aes(x=countries, y=sum_by_country, fill=countries)) +
       geom_bar(stat="identity", width=1)
     })
 }
