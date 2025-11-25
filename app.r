ce6e744444fc65f6e638ed9bcffd184f35db9359
Fox Walczak <fox.walczak2023@my.ntu.ac.uk>
2025-11-25 20:03:19 +0000

    fix,refactor,feat: functions, aggregates, test outputs, profit KPI
    fix and simplify functions
    
    refactor,fix: do aggregates for each line chart
    
    test: add test outputs
    
    feat: add profit KPI

--- app.r
+++ app.r
@@ -10,36 +10,27 @@ library("tidyverse")    # Core
 load_data <- function() {
   online_retail <- readr::read_csv("OnlineRetail.csv")
  
-  dplyr::rename(online_retail, raw_date = InvoiceDate)
-  online_retail$date <- as.Date(online_retail$raw_date, format="%m/%d/%Y %H:%M")
-  online_retail$date_time <- lubridate::as_datetime(online_retail$raw_date, format="%m/%d/%Y %H:%M")
+  online_retail$date_time <- lubridate::as_datetime(online_retail$InvoiceDate, format="%m/%d/%Y %H:%M")
+  online_retail$InvoiceDate <- as.Date(online_retail$InvoiceDate, format="%m/%d/%Y %H:%M")
  
   online_retail$InvoiceAmount <- online_retail$Quantity * online_retail$UnitPrice
-
+  
   return(online_retail)
 }
 
 # Split Costs and Revenue
-get_costs <- function(df){
-  return(df %>%
-           dplyr::pull(InvoiceAmount) %>%
-           dplyr::filter(InvoiceAmount < 0)
-         )}
-get_revenue <- function(df){
-  return(df %>%
-           dplyr::pull(InvoiceAmount) %>%
-           dplyr::filter(InvoiceAmount > 0)
-         )}
-min_date <- function(df) {
-  return(df %>%
-           dplyr::pull(date) %>%
-           min()
-  )}
-max_date <- function(df) {
-  return(df %>%
-           dplyr::pull(date) %>%
-           max()
-  )}
+filter_cost <- function(df){return(
+  dplyr::filter(df, InvoiceAmount < 0) %>%
+    dplyr::mutate(InvoiceAmount = abs(InvoiceAmount))
+)}
+filter_revenue <- function(df){ return(dplyr::filter(df, InvoiceAmount > 0)) }
+
+# Profit
+calculate_profit <- function(df){ return(sum(dplyr::pull(df,InvoiceAmount))) }
+
+# Get Min/Max Date
+min_date <- function(df) { return(min(dplyr::pull(df,InvoiceDate))) }
+max_date <- function(df) { return(max(dplyr::pull(df,InvoiceDate))) }
 
 # UI
 
@@ -95,33 +86,69 @@ ui <- shiny::navbarPage(
 # SERVER
 
 server <- function(input, output) {
-  reactive({
-    load_data()
-  })
+  online_retail <- load_data()
   rv <- reactiveValues()
   observe({
-    rv$min_date <- dplyr::pull(input, date_range[1])
-    rv$max_date <- dplyr::pull(input, date_range[2])
+    rv$min_date <- input$date_range[1]
+    rv$max_date <- input$date_range[2]
   })
   observe({
     # [O]nline [R]etail
-    rv$or <- online_retail %>% dplyr::filter(date >= rv$min_date &
-                                               date <= rv$max_date)
+    rv$or <- online_retail %>% dplyr::filter(InvoiceDate >= rv$min_date &
+                                               InvoiceDate <= rv$max_date)
   })
+  output$dates <- shiny::renderText({
+    paste(rv$min_date, rv$max_date)
+  })
+  output$ort <- shiny::renderTable({
+    as.data.frame(online_retail[1:10,])
+  })
+  output$rvort <- shiny::renderTable({
+    temp <- rv$or[1:10,]
+    temp$InvoiceDate <- format(temp$InvoiceDate)
+    as.data.frame(temp %>% dplyr::select(Description, InvoiceDate), stringsAsFactors=TRUE)
+  })
+  
+  # REVENUE / COST
+  
   output$revenue_over_time <- renderPlot({
-      ggplot(get_revenue(rv$or), mapping=aes(x=date_time,y=InvoiceAmount)) + geom_line()
+    ggplot(stats::aggregate(InvoiceAmount~InvoiceDate,filter_revenue(rv$or), sum),
+           mapping=aes(x=InvoiceDate,y=InvoiceAmount)) +
+      geom_line(colour="green")
   })
   output$costs_over_time <- renderPlot({
-    ggplot(get_costs(rv$or), mapping=aes(x=date_time,y=InvoiceAmount)) + geom_line()
+    ggplot(stats::aggregate(InvoiceAmount~InvoiceDate,filter_cost(rv$or), sum),
+           mapping=aes(x=InvoiceDate,y=InvoiceAmount)) +
+      geom_line(colour="red")
   })
-  output$money_by_location <- renderPlot({
-    ggplot(rv$or, mapping=aes(x=date_time,y=InvoiceAmount,colour=Country)) + geom_line()
+  output$money_over_time <- renderPlot({
+             #mutate(InvoiceAmount = dplyr::case_when(
+             #  InvoiceAmount < 0 ~ 0-InvoiceAmount,
+             #  InvoiceAmount >= 0 ~ InvoiceAmount)) %>%
+    stats::aggregate(InvoiceAmount~InvoiceDate+(InvoiceAmount<0), rv$or, sum) %>% ggplot(
+      #mapping=aes(x=InvoiceDate,y=InvoiceAmount)) +
+      mapping=aes(x=InvoiceDate,y=abs(InvoiceAmount), colour=InvoiceAmount<0)) +
+      scale_colour_discrete(name="Key",labels=c("Revenue","Costs"), palette=c("green","red")) +
+      geom_line()
+    #ggplot(stats::aggregate(InvoiceAmount~InvoiceDate, rv$or, sum) %>%
+    #         mutate(InvoiceAmount = dplyr::case_when(
+    #           InvoiceAmount < 0 ~ 0-InvoiceAmount,
+    #           InvoiceAmount >= 0 ~ InvoiceAmount)),
+    #       mapping=aes(x=InvoiceDate,y=InvoiceAmount)) +
+    #       #mapping=aes(x=InvoiceDate,y=InvoiceAmount, colour=InvoiceAmount<0)) +
+    #  geom_line()
   })
-  output$revenue_by_country <- renderPlot({
-    country_data_frame <- stats::aggregate(InvoiceAmount~Country, get_revenue(rv$or), sum)
-    ggplot(country_data_frame, mapping=aes(x=countries, y=sum_by_country, fill=countries)) +
-      geom_bar(stat="identity", width=1)
-    })
+  #output$money_by_location <- renderPlot({
+  #  ggplot(rv$or, mapping=aes(x=date_time,y=InvoiceAmount,colour=Country)) + geom_line()
+  #})
+  #output$revenue_by_country <- renderPlot({
+  #  country_data_frame <- stats::aggregate(InvoiceAmount~Country, filter_revenue(rv$or), sum)
+  #  ggplot(country_data_frame, mapping=aes(x=countries, y=sum_by_country, fill=countries)) +
+  #    geom_bar(stat="identity", width=1)
+  #})
+  
+  # KPIs
+  output$profit <- renderText({calculate_profit(rv$or)})
 }
 
 # RUN
